ae0893abf6f0595a74b77117d4dcbe2a8c9c6e31
François LE GALL <francois.legal@hotmail.fr>
2025-02-28 12:40:22 +0000

    stg_customers

---
+++ models/staging/stg_customers.sql
@@ -0,0 +1,16 @@
+with customers as (
+    
+    select
+        customer_id,
+        first_name,
+        last_name,
+        phone,
+        email,
+        street,
+        city,
+        state,
+        zip_code
+    from {{source("local_bike","customers")}}
+)
+
+select * from customers
